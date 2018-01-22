3a4b95b2684baed156c7f1190dedbbfb428bf9b3
HarriMaatta <harri.maatta@elisa.fi>
2018-01-22 13:42:26 +0200

    Update harri-maatta.js

--- src/organizers/harri-maatta.js
+++ src/organizers/harri-maatta.js
@@ -1,12 +1,12 @@
 module.exports = {
   name: "Harri Määttä",
-  about: "",
+  about: "From Symbian/Qt developer to DevOps Special. Nowadays also GitHub Enterprise admin. And no, being GitHub admin doesn't bring new friends for you. Please believe me ;)",
   image: "harri.jpg",
   social: {
     homepage: "",
     twitter: "HarriMaatta",
-    github: "",
-    linkedin: "",
+    github: "HarriMaatta",
+    linkedin: "harrimaatta",
   },
   location: {
     country: {
